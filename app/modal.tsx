c1cf7cc50cdf927d72f506d39ccb6fd9d6a18b26
Ayan Pal <APal@ESTUATE.COM>
2025-10-31 10:37:45 +0530

    first commit

--- app/modal.tsx
+++ app/modal.tsx
@@ -15,7 +15,7 @@ export default function ModalScreen() {
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create({   
   container: {
     flex: 1,
     alignItems: 'center',
